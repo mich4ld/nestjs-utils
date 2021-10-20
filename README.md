41132bd6955fc6ffe7764232cdbd1d63f471ae4c
mich4ld <mchldziuba@gmail.com>
2021-10-20 17:09:17 +0200

    expand readme.md and export timeout interceptor

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 My personal utils for Nest.js applications
 
 It contains custom pipes, decorators etc.
+Most part of code you can find in official Nest.js docs.
+
+### Installation
+
+#### Yarn
+```bash
+yarn add @mich4l/nestjs-utils
+```
+
+#### npm
+```bash
+npm i @mich4l/nestjs-utils
+```
+
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// Pipes:
 export { JoiPipe, Validator } from './pipes/joi.pipe';
+// Decorators:
 export { User } from './decorators/user.decorator';
 export { Cookies } from './decorators/cookies.decorator';
+// Interceptors:
+export { TimeoutInterceptor } from './interceptors/timeout.interceptor';
